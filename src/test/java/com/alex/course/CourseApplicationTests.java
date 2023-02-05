75655919df1af18b278d0f66426be0e7e711e601
Alex Sandro <alex.sandro.1199@gmail.com>
2023-02-05 09:31:37 -0300

    Project created

---
+++ src/test/java/com/alex/course/CourseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.alex.course;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CourseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

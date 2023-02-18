841ee95098531d4d2ef65e2880e8bf3d4cdbdff4
Alex Sandro <alex.sandro.1199@gmail.com>
2023-02-18 09:13:21 -0300

    Exception handling - delete

---
+++ src/main/java/com/alex/course/services/exceptions/DatabaseException.java
@@ -0,0 +1,8 @@
+package com.alex.course.services.exceptions;
+
+public class DatabaseException extends RuntimeException {
+
+    public DatabaseException(String message) {
+        super(message);
+    }
+}
